cec0293d3cef21a4fb08a6a8d6374437280b543a
khchoi <khchoi0301@gmail.com>
2018-11-23 11:08:45 +0900

    make the feature which creates wallet

--- BlockPang/blockpang/faucet-app/views.py
+++ BlockPang/blockpang/faucet-app/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
 import urllib.request
 
 from iconsdk.wallet.wallet import KeyWallet
@@ -15,87 +14,96 @@ from iconsdk.builder.transaction_builder import (
     MessageTransactionBuilder
 )
 
-
 default_score = "cx31aef86872e344658372bd23a3cbbc1c810ec8fa"
 # icon_service = IconService(HTTPProvider("https://bicon.net.solidwallet.io/api/v3"))
 icon_service = IconService(HTTPProvider("http://127.0.0.1:9000/api/v3"))
 
 
-wallet = KeyWallet.load("/home/khchoi/icon-tut/BlockPang/BlockPang/blockpang/faucet-app/keystore_test1.json", "@icon111")
+wallet = KeyWallet.load(
+    "/home/khchoi/icon-tut/BlockPang/BlockPang/blockpang/faucet-app/keystore_test1.json", "@icon111")
 wallet_from = wallet.get_address()
 # wallet_from ='hx9920fb964da4047c3bd86a6263a62ae2e63e6c96'
-wallet_to ='hxa039d2a3f908ff83de04f8cfe893277eed0c97f0'
+wallet_to = 'hxa039d2a3f908ff83de04f8cfe893277eed0c97f0'
 
 
 def index(request):
-    
+
     page = '<div>faucet/wallet_address</div>'
     return HttpResponse(page)
     # return HttpResponse('Bye')
 
-# def test(request):
-#   return HttpResponse('test')
 
-def req_icx(request,to_address):
+def createwallet(request):
 
-    wallet_to =to_address
+    wallet = KeyWallet.create()
+    new_wallet = {}
+    # Check the wallet address
+    new_wallet['address'] = wallet.get_address()
+    # Let try getting the private key
+    new_wallet['key'] = wallet.get_private_key()
+
+    return HttpResponse(str(new_wallet))
+
+
+def req_icx(request, to_address):
+
+    wallet_to = to_address
 
     # Transfer
     transaction = CallTransactionBuilder()\
-    .from_(wallet.get_address())\
-    .to(default_score)\
-    .step_limit(5000000)\
-    .nid(3)\
-    .nonce(100)\
-    .method("send_icx")\
-    .params({'_from':wallet_from,'_to':wallet_to})\
-    .build()
+        .from_(wallet.get_address())\
+        .to(default_score)\
+        .step_limit(5000000)\
+        .nid(3)\
+        .nonce(100)\
+        .method("send_icx")\
+        .params({'_from': wallet_from, '_to': wallet_to})\
+        .build()
     # Returns the signed transaction object having a signature
     signed_transaction = SignedTransaction(transaction, wallet)
     # Sends the transaction
     tx_hash = icon_service.send_transaction(signed_transaction)
     print('transaction complete')  # added
-    
+
     # Address of the Block
     call = CallBuilder().from_(wallet_from)\
-    .to(default_score)\
-    .method("get_address")\
-    .build()
+        .to(default_score)\
+        .method("get_address")\
+        .build()
     get_address = icon_service.call(call)
-    print('get_address',get_address)
+    print('get_address', get_address)
 
     # Balance of the Block
     call = CallBuilder().from_(wallet_from)\
-    .to(default_score)\
-    .method("get_balance")\
-    .build()
+        .to(default_score)\
+        .method("get_balance")\
+        .build()
     get_balance = icon_service.call(call)
-    print('get_balance',get_balance)
+    print('get_balance', get_balance)
 
     # Balance of to_Address
     call = CallBuilder().from_(wallet_from)\
-    .to(default_score)\
-    .method("get_to")\
-    .params({'_from':wallet_from,'_to':wallet_to})\
-    .build()
+        .to(default_score)\
+        .method("get_to")\
+        .params({'_from': wallet_from, '_to': wallet_to})\
+        .build()
     get_to = icon_service.call(call)
-    print('get_to',wallet_to,get_to)
-
+    print('get_to', wallet_to, get_to)
 
     call = CallBuilder().from_(wallet_from)\
-    .to(default_score)\
-    .method("find_transaction")\
-    .params({'_to':wallet_to})\
-    .build()
+        .to(default_score)\
+        .method("find_transaction")\
+        .params({'_to': wallet_to})\
+        .build()
     find_transaction = icon_service.call(call)
-    print('find_transaction',find_transaction)
+    print('find_transaction', find_transaction)
 
     call = CallBuilder().from_(wallet_from)\
-    .to(default_score)\
-    .method("block_height")\
-    .build()
+        .to(default_score)\
+        .method("block_height")\
+        .build()
     block_height = icon_service.call(call)
-    print('block_height',block_height)
+    print('block_height', block_height)
 
     # call = CallBuilder().from_(wallet_from)\
     # .to(default_score)\
@@ -104,5 +112,8 @@ def req_icx(request,to_address):
     # get_block = icon_service.call(call)
     # print('get_block',get_block)
 
-    page = 'block height : '+str(int(block_height,16))+'<div></div>'+' find_transaction : '+str(int(find_transaction,16))+'<div>tx_hash : </div>'+tx_hash + '<div></div>'+'block:'+'<div></div>'+get_address +' // block balance'+ str(int(get_balance,16)/10**18) +'<div>to:</div>'+ str(wallet_to)+' // balance is '+ str(int(get_to,16)/10**18)
+    page = 'block height : '+str(int(block_height, 16))+'<div></div>'+' find_transaction : '+str(int(find_transaction, 16))+'<div>tx_hash : </div>'+tx_hash + '<div></div>'+'block:' + \
+        '<div></div>'+get_address + ' // block balance' + \
+        str(int(get_balance, 16)/10**18) + '<div>to:</div>' + \
+        str(wallet_to)+' // balance is ' + str(int(get_to, 16)/10**18)
     return HttpResponse(page)
--- SCORE_testnet/my_testnet_score/my_testnet_score.py
+++ SCORE_testnet/my_testnet_score/my_testnet_score.py
@@ -1,26 +1,27 @@
 from iconservice import *
-import json
 
 TAG = 'TestnetScore'
+
 
 class TestnetScore(IconScoreBase):
 
     _DISPENSE_TRACK = "DISPENSE TRACK"
-    
+
     @eventlog(indexed=2)
     def FundTransfer(self, backer: Address, amount: int, is_top_up: bool):
         pass
 
     def __init__(self, db: IconScoreDatabase) -> None:
         super().__init__(db)
-        self._dispenseTracking = DictDB(self._DISPENSE_TRACK, db, value_type=int)
+        self._dispenseTracking = DictDB(
+            self._DISPENSE_TRACK, db, value_type=int)
 
     def on_install(self) -> None:
         super().on_install()
 
     def on_update(self) -> None:
         super().on_update()
-    
+
     @external(readonly=True)
     def hello(self) -> str:
         Logger.debug(f'Hello, world!', TAG)
@@ -30,61 +31,82 @@ class TestnetScore(IconScoreBase):
     def get_balance(self) -> str:
         return self.icx.get_balance(self.address)
 
-
-    # it doesn't work // why?? callBuilder problem???
-    @external(readonly=True)   
-    def get_block(self) -> str:
-        return str(self.icx.get_block("latest"))
-        
-    @external(readonly=True)   
+    @external(readonly=True)
     def block_height(self) -> str:
         return self.block.height
 
     @external(readonly=True)
-    def find_transaction(self, _to:Address) -> str:
+    def find_transaction(self, _to: Address) -> str:
         return self._dispenseTracking[_to]
-
 
     @external(readonly=True)
     def get_address(self) -> str:
         return self.address
 
-
     @external(readonly=True)
     def get_to(self, _to: Address, _from: Address) -> str:
         return self.icx.get_balance(_to)
 
+    @external(readonly=True)
+    def createwallet(self):
+        # wallet = KeyWallet.create()
+        # new_wallet = {}
+
+        # Check the wallet address
+        # new_wallet['address'] = wallet.get_address()
+        # new_wallet = wallet.get_address()
+
+        # Let try getting the private key
+        # new_wallet['key'] = wallet.get_private_key()
+
+        # return str(new_wallet)
+
+    @external(readonly=True)  # it doesn't work // why?? callBuilder problem???
+    def get_block(self) -> str:
+        return self.get_block('0xee0019ead377d6f9ee560c669f66b6a13811761c2388682d66a0f253cfbcdb24')
+
+    # @external(readonly=True)
+    # def get_block(self) -> str:
+    #     return str(self.icx.get_block("6934"))
+    #     return str(self.icx.get_block("latest"))
+    #     return str(self.get_block(6934))
+    #     return str(self.icx.get_block(6934))
+    #     return self.icx.get_block(6934)
+    #     return self.icx.get_block('0xee0019ead377d6f9ee560c669f66b6a13811761c2388682d66a0f253cfbcdb24')
 
     @external
     def send_icx(self, _to: Address, _from: Address):
 
-        if self._dispenseTracking[_to] > 0 and (self.block.height < self._dispenseTracking[_to] +5):
-            Logger.info(f'Please wait for 30 blocks to be created before requesting again', TAG)
+        if self._dispenseTracking[_to] > 0 and (self.block.height < self._dispenseTracking[_to] + 5):
+            Logger.info(
+                f'Please wait for 30 blocks to be created before requesting again', TAG)
             revert('Please wait for 30 blocks to be created before requesting again')
 
         amount = 5 * 10 ** 17
 
         result = self.icx.transfer(_to, amount)
-        Logger.debug(f'result of self.icx.send for amount {amount} to {_to} is {result}', TAG)
+        Logger.debug(
+            f'result of self.icx.send for amount {amount} to {_to} is {result}', TAG)
         self.FundTransfer(_to, amount, False)
         Logger.debug(f'{amount} ICX sent to {_to} ', TAG)
         self._dispenseTracking[_to] = self.block.height
         return result
 
+    # @external
+    # @payable
+    # def send_icx_p(self, _to: Address, _from: Address):  # does'n work
+    #     amount = 5 * 10 ** 17
 
-    @external
-    @payable
-    def send_icx_p(self, _to: Address, _from: Address):
-        amount = 5 * 10 ** 17
-
-        result = self.icx.transfer(_to, amount)
-        Logger.debug(f'result of self.icx.send for amount {amount} to {_to} is {result}', TAG)
-        self.FundTransfer(_to, amount, False)
-        Logger.debug(f'{amount} ICX sent to {_to} ', TAG)
-        return result
+    #     result = self.icx.transfer(_to, amount)
+    #     Logger.debug(
+    #         f'result of self.icx.send for amount {amount} to {_to} is {result}', TAG)
+    #     self.FundTransfer(_to, amount, False)
+    #     Logger.debug(f'{amount} ICX sent to {_to} ', TAG)
+    #     return result
 
     @payable
     def fallback(self):
-        #topup ICX
-        Logger.debug(f'faucet topup from {self.msg.sender} with amount {self.msg.value}', TAG)
+        # topup ICX
+        Logger.debug(
+            f'faucet topup from {self.msg.sender} with amount {self.msg.value}', TAG)
         return 'fallback'
