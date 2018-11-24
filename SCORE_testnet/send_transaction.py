22411b6755f414d7d2b9214af22e52648803057b
khchoi <khchoi0301@gmail.com>
2018-11-24 19:45:39 +0900

    make server get POST requests

--- SCORE_testnet/send_transaction.py
+++ SCORE_testnet/send_transaction.py
@@ -1,16 +1,18 @@
 # send_transaction.py
-# Load our existing wallet 1 
-from iconsdk.wallet.wallet import KeyWallet
-wallet = KeyWallet.load("./iconkeystore", "@icon111")
-# Build a transaction instance, hard-code it to send 1 ICX from wallet 1 to wallet 2
+# Load our existing wallet 1
+from iconsdk.providers.http_provider import HTTPProvider
+from iconsdk.icon_service import IconService
+from iconsdk.signed_transaction import SignedTransaction
 from iconsdk.builder.transaction_builder import (
     TransactionBuilder,
     DeployTransactionBuilder,
     CallTransactionBuilder,
     MessageTransactionBuilder
 )
+from iconsdk.wallet.wallet import KeyWallet
+wallet = KeyWallet.load("./iconkeystore", "@icon111")
+# Build a transaction instance, hard-code it to send 1 ICX from wallet 1 to wallet 2
 
-from iconsdk.signed_transaction import SignedTransaction
 transaction = TransactionBuilder()\
     .from_(wallet.get_address())\
     .to("hxc425d7a44a76b17245528ec4225be1e6a2f8635d")\
@@ -19,9 +21,8 @@ transaction = TransactionBuilder()\
     .nid(3)\
     .nonce(100)\
     .build()
-from iconsdk.icon_service import IconService
-from iconsdk.providers.http_provider import HTTPProvider
-icon_service = IconService(HTTPProvider("https://bicon.net.solidwallet.io/api/v3"))
+icon_service = IconService(HTTPProvider(
+    "https://bicon.net.solidwallet.io/api/v3"))
 # Returns the signed transaction object having a signature
 signed_transaction = SignedTransaction(transaction, wallet)
 # Sends the transaction
