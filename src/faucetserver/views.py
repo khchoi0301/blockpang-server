db814bffd656c4263f17290da9bfe5a3d9cf3c4d
Sung Un Kim <sungunkim367@gmail.com>
2018-12-03 10:16:09 +0900

    remove api access restriction for testing

--- src/faucetserver/views.py
+++ src/faucetserver/views.py
@@ -39,19 +39,16 @@ def index(request):
     return JsonResponse({'admin_email': staff_list})
 
 
-@staff_member_required
 def db_query(request, table):
     return JsonResponse(utils.db_query(request, table), safe=False)
 
 
-@staff_member_required
 @csrf_exempt
 def update_admin(request):
     if request.method == 'POST':
         return JsonResponse(utils.update_admin(request), safe=False)
 
 
-@staff_member_required
 def get_current_balance(request):
     return JsonResponse({
         'default_score': default_score, 
@@ -81,7 +78,6 @@ def transfer_stat(request):
     return JsonResponse(utils.transfer_stat(request), safe=False)
 
 
-@staff_member_required
 @csrf_exempt  # need to think about security
 def user_stat(request):
     return JsonResponse(utils.user_stat(request))
@@ -91,7 +87,6 @@ def set_limit(request, amount_limit, block_limit):
     return JsonResponse(utils.set_limit(request, amount_limit, block_limit))
 
 
-@staff_member_required
 def get_limit(request):
     limit = utils.get_limit()
     limit['amountlimit'] = int(limit['amountlimit'], 16) / 10 ** 18
