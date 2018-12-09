df45b2478550fa5c5a09b586e31a756e5373c2ae
Sung Un Kim <sungunkim367@gmail.com>
2018-12-09 23:30:11 +0900

    remove csrf_exempt import

--- src/faucetserver/utils_admin.py
+++ src/faucetserver/utils_admin.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.mail import send_mail
 from django.db import IntegrityError
-from django.views.decorators.csrf import csrf_exempt
 import os
 import re
 import time
@@ -37,7 +36,6 @@ wallet_from = settings.WALLET_FROM
 
 
 # Check admin username and password, and then give token
-@csrf_exempt
 @api_view(['POST'])
 @permission_classes((AllowAny,))
 def get_token(request):
